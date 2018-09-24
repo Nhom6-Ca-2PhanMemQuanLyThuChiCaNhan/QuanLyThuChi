4a46d92861627f8311815807926557236e24d820
thanhkhoatran <35222306+thanhkhoatran@users.noreply.github.com>
2018-09-25 02:48:41 +0700

    Revert "Revert "Init""
    This reverts commit 1977436ef8b1d9c93ae66260b515ca345835bbac.

---
+++ QuanLyThuChi/QuanLyThuChi/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace QuanLyThuChi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.lb_user = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.lb_pass = new System.Windows.Forms.Label();
+            this.btn_dn = new System.Windows.Forms.Button();
+            this.btn_huy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Username.Location = new System.Drawing.Point(133, 49);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(219, 23);
+            this.txt_Username.TabIndex = 1;
+            // 
+            // lb_user
+            // 
+            this.lb_user.AutoSize = true;
+            this.lb_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_user.Location = new System.Drawing.Point(130, 17);
+            this.lb_user.Name = "lb_user";
+            this.lb_user.Size = new System.Drawing.Size(105, 17);
+            this.lb_user.TabIndex = 1;
+            this.lb_user.Text = "Tên đăng nhập";
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.Location = new System.Drawing.Point(133, 121);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(219, 23);
+            this.txt_Password.TabIndex = 2;
+            // 
+            // lb_pass
+            // 
+            this.lb_pass.AutoSize = true;
+            this.lb_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_pass.Location = new System.Drawing.Point(130, 89);
+            this.lb_pass.Name = "lb_pass";
+            this.lb_pass.Size = new System.Drawing.Size(66, 17);
+            this.lb_pass.TabIndex = 1;
+            this.lb_pass.Text = "Mật khẩu";
+            // 
+            // btn_dn
+            // 
+            this.btn_dn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dn.Location = new System.Drawing.Point(133, 161);
+            this.btn_dn.Name = "btn_dn";
+            this.btn_dn.Size = new System.Drawing.Size(86, 28);
+            this.btn_dn.TabIndex = 3;
+            this.btn_dn.Text = "Đăng nhập";
+            this.btn_dn.UseVisualStyleBackColor = true;
+            this.btn_dn.Click += new System.EventHandler(this.btn_dn_Click);
+            // 
+            // btn_huy
+            // 
+            this.btn_huy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_huy.Location = new System.Drawing.Point(266, 161);
+            this.btn_huy.Name = "btn_huy";
+            this.btn_huy.Size = new System.Drawing.Size(86, 28);
+            this.btn_huy.TabIndex = 4;
+            this.btn_huy.Text = "Hủy";
+            this.btn_huy.UseVisualStyleBackColor = true;
+            this.btn_huy.Click += new System.EventHandler(this.btn_huy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 226);
+            this.Controls.Add(this.btn_huy);
+            this.Controls.Add(this.btn_dn);
+            this.Controls.Add(this.lb_pass);
+            this.Controls.Add(this.lb_user);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_Username);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Label lb_user;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label lb_pass;
+        private System.Windows.Forms.Button btn_dn;
+        private System.Windows.Forms.Button btn_huy;
+    }
+}
+
